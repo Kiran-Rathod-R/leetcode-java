81a38fa5063d4d87562eef85a4f9d72b37f143eb
Kiran Rathod <rathodkiran892@gmail.com>
2026-08-21 21:39:54 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.2%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        int n = digits.length;
+
+        if (digits[n - 1] != 9) {
+            digits[n - 1] += 1;
+
+            return digits;
+        }
+        
+            for(int i=n-1;i>=0;i--){
+                if(digits[i]!=9){
+                    digits[i]+=1;
+
+                    return digits;
+                }
+                else{
+                    digits[i]=0;
+
+                }
+            }
+
+            if(digits[0]==0){
+                int[] arr=new int[n+1];
+
+                arr[0]=1;
+                for(int i=1;i<n+1;i++){
+                    arr[i]=digits[i-1];
+                }
+                return arr;
+            }
+        
+        return digits;
+
+    }
+}
